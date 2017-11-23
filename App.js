e60f95b21be6b5a804bd098f5a999fde33fa036c
Krzysztof Karol <krzysztof.karol@gmail.com>
2017-11-23 22:55:39 +0100

    Add FlatList

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {FlatList, StyleSheet, Text, View} from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <FlatList
+          data={[
+            {key: 'First Thing'},
+          ]}
+          renderItem={({item}) => <Text>{item.key}</Text>}
+          style={styles.list}
+        />
       </View>
     );
   }
@@ -20,4 +24,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  list: {
+    marginTop: 30,
+  }
 });
